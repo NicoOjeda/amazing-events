26db99c9eed64e406dc9efabebcaf5eb85611908
Nico ojeda <nico_ojeda86@hotmail.com>
2022-08-05 16:21:07 -0300

    details primera version

---
+++ details.js
@@ -0,0 +1,27 @@
+let datos = data.events;
+console.log(window);
+
+const queryString = location.search
+const params = new URLSearchParams(queryString) 
+const id = parseInt(params.get("id"))
+
+console.log(id);
+
+let persona = datos.find(item=> item._id == id)
+console.log(persona);
+
+let cajaBig = document.querySelector("#cardBig") 
+cajaBig.innerHTML = `<div class="cajacard2 p-1">
+<img src="${persona.image}" class="imgcard2" alt="concierto">
+</div>
+<div class="card-body cajatexto">
+<p class="card-text p-1">Name: ${persona.name}</p>
+<p class="card-text p-1">Date: ${persona.date}</p>
+<p class="card-text p-1">Description: ${persona.description}</p>
+<p class="card-text p-1">Category: ${persona.category}</p>
+<p class="card-text p-1">Place: ${persona.place}</p>
+<p class="card-text p-1">Capacity: ${persona.capacity}</p>
+<p class="card-text p-1 primeraletra">${Object.keys(persona)[8]}: ${persona.assistance || persona.estimate} </p>
+<p class="card-text p-1">Price: ${persona.price}</p>
+</div>`
+
